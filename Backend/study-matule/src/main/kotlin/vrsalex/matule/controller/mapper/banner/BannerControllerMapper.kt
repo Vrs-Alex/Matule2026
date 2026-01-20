8a5c7cac5dd996c65459479962bc1de75bb415df
Vrs-Alex <alexgm0508@gmail.com>
2026-01-20 21:09:55 +0300

    Updated insomnia config

--- Backend/study-matule/src/main/kotlin/vrsalex/matule/controller/mapper/banner/BannerControllerMapper.kt
+++ Backend/study-matule/src/main/kotlin/vrsalex/matule/controller/mapper/banner/BannerControllerMapper.kt
@@ -1,10 +1,11 @@
 package vrsalex.matule.controller.mapper.banner
 
+import vrsalex.matule.api.endpoints.ServerEndpoints
 import vrsalex.matule.api.response.banner.BannerResponse
 import vrsalex.matule.domain.model.Banner
 
 fun Banner.toResponse() = BannerResponse(
     id = this.id!!,
     title = this.title,
-    image = this.image
+    image = ServerEndpoints.API.IMAGES_PREFIX + "/" + this.image,
 )
