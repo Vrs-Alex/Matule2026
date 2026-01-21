8662e47d32df7b6da836d73669e70e01c0ba6722
Vrs-Alex <alexgm0508@gmail.com>
2026-01-21 15:19:31 +0300

    fix(network) replace string endpoints to ServerEnpoints in AuthApi

--- Api/network/src/main/java/com/vrsalex/network/internal/api/AuthApi.kt
+++ Api/network/src/main/java/com/vrsalex/network/internal/api/AuthApi.kt
@@ -5,6 +5,7 @@ import com.vrsalex.network.api.dto.request.auth.RefreshTokenRequest
 import com.vrsalex.network.api.dto.request.auth.SignInRequest
 import com.vrsalex.network.api.dto.request.auth.SignUpRequest
 import com.vrsalex.network.api.dto.response.AuthResponse
+import com.vrsalex.network.internal.common.ServerEndpoints
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -13,16 +14,16 @@ import retrofit2.http.POST
 
 internal interface AuthApi {
 
-    @POST("auth/login")
+    @POST(ServerEndpoints.AUTH_USER_LOGIN_ENDPOINT)
     suspend fun login(@Body request: SignInRequest): Response<AuthResponse>
 
-    @POST("auth/signup")
+    @POST(ServerEndpoints.AUTH_USER_REGISTER_ENDPOINT)
     suspend fun signup(@Body request: SignUpRequest): Response<AuthResponse>
 
-    @GET("auth/logout")
+    @GET(ServerEndpoints.AUTH_USER_LOGOUT_ENDPOINT)
     suspend fun logout(): Response<Unit>
 
-    @POST("auth/update_tokens")
+    @POST(ServerEndpoints.AUTH_REFRESH_TOKEN_ENDPOINT)
     suspend fun updateTokens(@Body request: RefreshTokenRequest): Response<AuthResponse>
 
 }
