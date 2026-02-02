b9c2257d388c692ccddd3c2c59ff2349a492b7fa
Vrs-Alex <alexgm0508@gmail.com>
2026-02-03 00:15:08 +0300

    fix(profile) add intent for view pdf

--- Application/app/src/main/java/com/vrsalex/matuleapp/presentation/feature/profile/ProfileViewModel.kt
+++ Application/app/src/main/java/com/vrsalex/matuleapp/presentation/feature/profile/ProfileViewModel.kt
@@ -1,28 +1,32 @@
 package com.vrsalex.matuleapp.presentation.feature.profile
 
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.core.content.FileProvider
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.vrsalex.matuleapp.domain.profile.ProfileRepository
 import com.vrsalex.matuleapp.domain.setting.SettingRepository
 import com.vrsalex.network.api.common.AuthObserver
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import io.appmetrica.analytics.AppMetrica
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
+import java.io.File
 import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
     private val profileRepository: ProfileRepository,
     private val authObserver: AuthObserver,
-    private val settingRepository: SettingRepository
+    private val settingRepository: SettingRepository,
+    @ApplicationContext private val context: Context
 ): ViewModel() {
 
 
@@ -60,12 +64,26 @@ class ProfileViewModel @Inject constructor(
             }
             ProfileContract.Event.OnPrivacyPolicy -> {
                 viewModelScope.launch {
-                    _channel.send(ProfileContract.Effect.OnPrivacyPolicy("https://google.com"))
+//                    _channel.send(ProfileContract.Effect.OnPrivacyPolicy("https://google.com"))
+                    val tempFile = File(context.cacheDir, "policy.pdf")
+                    context.assets.open("policy.pdf").use { inputStream ->
+                        tempFile.outputStream().use { outputStream ->
+                            inputStream.copyTo(outputStream)
+                        }
+                    }
+                    val publicUri = FileProvider.getUriForFile(context, "${context.packageName}.fileprovider", tempFile)
+                    Log.e("MYAPP", publicUri.toString())
+                    val intent = Intent(Intent.ACTION_VIEW).apply {
+                        setDataAndType(publicUri, "application/pdf")
+                    }.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    context.startActivity(intent)
+
                 }
             }
             ProfileContract.Event.OnTermsOfUse -> {
                 viewModelScope.launch {
-                    _channel.send(ProfileContract.Effect.OnPrivacyPolicy("https://google.com"))
+//                    _channel.send(ProfileContract.Effect.OnPrivacyPolicy("https://google.com"))
                 }
             }
         }
