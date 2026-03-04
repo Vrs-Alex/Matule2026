1e3ca983d95ff689010a5b501c99ae60491a4d53
Vrs-Alex <alexgm0508@gmail.com>
2026-03-04 18:44:13 +0300

    fix(validation): change email validation from Android.PATTERN to Regex

--- Application/app/src/main/java/com/vrsalex/matuleapp/domain/validation/EmailValidation.kt
+++ Application/app/src/main/java/com/vrsalex/matuleapp/domain/validation/EmailValidation.kt
@@ -4,8 +4,11 @@ import android.util.Patterns
 
 object EmailValidation {
 
+    private val emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[a-z]{2,6}$".toRegex()
+
     fun validateEmail(email: String): String? {
-        val isValid = Patterns.EMAIL_ADDRESS.matcher(email).matches()
+//        val isValid = Patterns.EMAIL_ADDRESS.matcher(email).matches()
+        val isValid = emailRegex.matches(email)
         if (!isValid) {
             return "Некорректный формат почты"
         }
